1047e9486976a9fca907039ff4fcec9ed180163d
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2020-10-16 16:51:32 -0300

    5 dia!

--- src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -23,8 +23,8 @@ export default function SelectMapPosition() {
     <View style={styles.container}>
       <MapView
         initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
+          latitude: -6.9869038,
+          longitude: -34.8322513,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
